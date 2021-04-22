f79d936e6a720a0c90bae1d9a26d233d89bfb3fc
zach-koors <67700062+zach-koors@users.noreply.github.com>
2021-04-22 15:13:50 -0400

    started js, added sounds

---
+++ script.js
@@ -0,0 +1,17 @@
+function sequencer(){
+  const kick = new Tone.Player("./sounds/kick.m4a").toMaster();
+  const snare = new Tone.Player("./sounds/snare.m4a").toMaster();
+  const hihat = new Tone.Player("./sounds/hihat.m4a").toMaster();
+  let index = 0;
+
+  const kickSteps = document.querySelectorAll(".k");
+  const snareSteps = document.querySelectorAll(".s");
+  const hihatSteps = document.querySelectorAll(".h");
+
+  Tone.Trasnport.scheduleRepeat(repeat, "8n");
+  function repeat(){
+    let step = index % 8;
+  }
+
+}
+sequencer();
