edf6e4eeb0e3b266de63c26620bde75ac5a47bd0
Zach Koors <koorsz@gmail.com>
2021-08-24 11:29:45 -0400

    finished initial design look, starting js

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
-function sequencer(){
-  const kick = new Tone.Player("./sounds/kick.m4a").toMaster();
-  const snare = new Tone.Player("./sounds/snare.m4a").toMaster();
-  const hihat = new Tone.Player("./sounds/hihat.m4a").toMaster();
-  let index = 0;
-
-  const kickSteps = document.querySelectorAll(".k");
-  const snareSteps = document.querySelectorAll(".s");
-  const hihatSteps = document.querySelectorAll(".h");
-
-  Tone.Trasnport.scheduleRepeat(repeat, "8n");
-  function repeat(){
-    let step = index % 8
-  }
-
-}
-sequencer();
+$(document).ready(function(){
+  $(".off").click(function(){
+    $(this).toggleClass("on");
+  });
+  $(".on").click(function(){
+    $(this).removeClass("on").addClass("on-loud");
+  });
+  $(".on-loud").click(function(){
+    $(this).removeClass("on-loud").addClass("off");
+  });
+});
