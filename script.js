776aa99a5627c804138b4490beee0add8f82a239
Zach Koors <koorsz@gmail.com>
2021-08-24 14:01:33 -0400

    melodic steps cycle 9 on's then off, rhythmic steps on on-loud off

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
   $(".rhythmic.step").click(function(){
-    let stepClasses = ['on','on-loud','off'];
+    let stepClasses = ['rhythmic step on','rhythmic step on-loud','rhythmic steo off'];
     $(this).each(function(){
       this.className = stepClasses[($.inArray(this.className, stepClasses)+1)%stepClasses.length];
     });
   });
   $(".melodic.step").click(function(){
-    let stepClasses = ['on','on-loud','off'];
+    let stepClasses = ['melodic step on 0','melodic step on 1','melodic step on 2','melodic step on 3','melodic step on 4','melodic step on 5','melodic step on 6','melodic step on 7','melodic step on 8','melodic step on 9','melodic step off'];
     $(this).each(function(){
       this.className = stepClasses[($.inArray(this.className, stepClasses)+1)%stepClasses.length];
     });
