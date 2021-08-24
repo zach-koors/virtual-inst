b4bf0a4d73472c64d49ea0ac20d1987fa06dbba2
Zach Koors <koorsz@gmail.com>
2021-08-24 14:26:29 -0400

    melodic steps cycle through nums rhythmic steps cycle through on on-loud

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ $(document).ready(function(){
     });
   });
   $(".melodic.step").click(function(){
+    let melodicNote = ['0','1','2','3','4','5','6','7','8','9','•']
+    $(this).each(function(){
+      this.textContent = melodicNote[($.inArray(this.textContent, melodicNote)+1)%melodicNote.length];
+    });
     let stepClasses = ['melodic step on 0','melodic step on 1','melodic step on 2','melodic step on 3','melodic step on 4','melodic step on 5','melodic step on 6','melodic step on 7','melodic step on 8','melodic step on 9','melodic step off'];
     $(this).each(function(){
       this.className = stepClasses[($.inArray(this.className, stepClasses)+1)%stepClasses.length];
