d97876e090abb9eb74b34aa6b160a6ba03db34a8
Zach Koors <koorsz@gmail.com>
2021-10-26 22:42:24 -0400

    added new sound files, refamiliarized myself with the JS

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
-import * as Tone from 'tone';
+const { Tone } = require("tone/build/esm/core/Tone");
 
-$(document).ready(function(){
-  $(".rhythmic.step").click(function(){
+function sequencer() {
+  const kick = new Tone.Player("./sounds/kick2.wav").toDestination();
+  const snare = new Tone.Player("./sounds/snare2.wav ").toDestination();
+  
+}
+
+$(document).ready(function() {
+  $(".rhythmic.step").click(function() {
     let stepClasses = ['rhythmic step on','rhythmic step on-loud','rhythmic step off'];
-    $(this).each(function(){
+    $(this).each(function() {
       this.className = stepClasses[($.inArray(this.className, stepClasses)+1)%stepClasses.length];
     });
   });
-  $(".melodic.step").click(function(){
+  $(".melodic.step").click(function() {
     let melodicNote = ['0','1','2','3','4','5','6','7','8','9','•']
-    $(this).each(function(){
+    $(this).each(function() {
       this.textContent = melodicNote[($.inArray(this.textContent, melodicNote)+1)%melodicNote.length];
     });
     let stepClasses = ['melodic step on 0','melodic step on 1','melodic step on 2','melodic step on 3','melodic step on 4','melodic step on 5','melodic step on 6','melodic step on 7','melodic step on 8','melodic step on 9','melodic step off'];
-    $(this).each(function(){
+    $(this).each(function() {
       this.className = stepClasses[($.inArray(this.className, stepClasses)+1)%stepClasses.length];
     });
   });
-}
+});
 
-,
-
-function sequencer(){
-  const sampler = new Tone.sampler
-}
-
-);
-
+sequencer();
