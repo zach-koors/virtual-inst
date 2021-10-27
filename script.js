cceff8955e4266f9799a1e802bf5c283e96f717a
Zach Koors <koorsz@gmail.com>
2021-10-27 17:19:29 -0400

    refactored/organized code, figured out JQuery for kick steps

--- script.js
+++ script.js
@@ -1,28 +1,69 @@
-const { Tone } = require("tone/build/esm/core/Tone");
-
-function sequencer() {
-  const kick = new Tone.Player("./sounds/kick2.wav").toDestination();
-  const snare = new Tone.Player("./sounds/snare2.wav ").toDestination();
-  
-}
-
 $(document).ready(function() {
+
   $(".rhythmic.step").click(function() {
-    let stepClasses = ['rhythmic step on','rhythmic step on-loud','rhythmic step off'];
+    
+    let stepClasses = [
+      'rhythmic step on',
+      'rhythmic step on-loud',
+      'rhythmic step off'
+    ];
+
     $(this).each(function() {
       this.className = stepClasses[($.inArray(this.className, stepClasses)+1)%stepClasses.length];
     });
   });
+
   $(".melodic.step").click(function() {
     let melodicNote = ['0','1','2','3','4','5','6','7','8','9','•']
     $(this).each(function() {
       this.textContent = melodicNote[($.inArray(this.textContent, melodicNote)+1)%melodicNote.length];
     });
-    let stepClasses = ['melodic step on 0','melodic step on 1','melodic step on 2','melodic step on 3','melodic step on 4','melodic step on 5','melodic step on 6','melodic step on 7','melodic step on 8','melodic step on 9','melodic step off'];
+
+    let stepClasses = [
+      'melodic step on 0',
+      'melodic step on 1',
+      'melodic step on 2',
+      'melodic step on 3',
+      'melodic step on 4',
+      'melodic step on 5',
+      'melodic step on 6',
+      'melodic step on 7',
+      'melodic step on 8',
+      'melodic step on 9',
+      'melodic step off'
+    ];
+
     $(this).each(function() {
       this.className = stepClasses[($.inArray(this.className, stepClasses)+1)%stepClasses.length];
     });
   });
-});
 
-sequencer();
+  function sequencer() {
+  const kick = new Tone.Player("./sounds/kick2.wav").toDestination();
+  const snare = new Tone.Player("./sounds/snare2.wav").toDestination();
+  const hihat = new Tone.Player("./sounds/hihat2.wav").toDestination();
+  const bass = new Tone.Player("./sounds/bass2.wav").toDestination();
+
+    let i = 0;
+
+   function loopThroughSteps() {
+    let step = i % 8;
+    let kickStep = $(".kick .step:nth-child(1)");
+    if(kickStep.hasClass("on")){
+      kick.stop();
+      kick.start();
+    }
+
+    console.log(step);
+
+    i++;
+  }
+
+  Tone.Transport.scheduleRepeat(loopThroughSteps, "8n");
+  //Tone.Transport.start();
+
+}
+
+  sequencer();
+
+});
