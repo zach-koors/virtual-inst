ef90cc6d8e5a330978785a3a4e1af9b092f51e9a
Zach Koors <koorsz@gmail.com>
2021-11-29 08:37:29 -0500

    fixed bug, curly brace was moved out of place and play button was not functioning properly

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
 const audioContext = new AudioContext;
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-  $("#play").click(function() {
-    if(audioContext.state === 'suspended'){
+  $("#play").click(function () {
+    if (audioContext.state === 'suspended') {
       audioContext.resume();
     }
     Tone.start();
   });
 
   //the function that enables the rhythmic step buttons to cycle through states when clicked
-  $(".rhythmic.step").click(function() {
+  $(".rhythmic.step").click(function () {
     let stepClasses = [
       'rhythmic step on',
       'rhythmic step on-loud',
       'rhythmic step off'
     ];
-    $(this).each(function() {
-      this.className = stepClasses[($.inArray(this.className, stepClasses)+1)%stepClasses.length];
+    $(this).each(function () {
+      this.className = stepClasses[($.inArray(this.className, stepClasses) + 1) % stepClasses.length];
     });
   });
 
   //the function that enables the melodic step buttons to cycle through pitches/states when clicked
-  $(".melodic.step").click(function() {
+  $(".melodic.step").click(function () {
     let melodicNote = [
-    'D',
-    'E',
-    'F',
-    'G',
-    'A',
-    'B',
-    'C',
-    'd',
-    'e',
-    'f',
-    '•'
+      'D',
+      'E',
+      'F',
+      'G',
+      'A',
+      'B',
+      'C',
+      'd',
+      'e',
+      'f',
+      '•'
     ];
-    $(this).each(function() {
-      this.textContent = melodicNote[($.inArray(this.textContent, melodicNote)+1)%melodicNote.length];
+    $(this).each(function () {
+      this.textContent = melodicNote[($.inArray(this.textContent, melodicNote) + 1) % melodicNote.length];
     });
 
 
@@ -54,8 +54,8 @@ $(document).ready(function() {
       'melodic step on 9',
       'melodic step off'
     ];
-    $(this).each(function() {
-      this.className = stepClasses[($.inArray(this.className, stepClasses)+1)%stepClasses.length];
+    $(this).each(function () {
+      this.className = stepClasses[($.inArray(this.className, stepClasses) + 1) % stepClasses.length];
     });
   });
 
@@ -81,7 +81,7 @@ $(document).ready(function() {
     }).toDestination();
     melody.volume.value = -6;
 
-    
+
 
     let i = 0;
 
@@ -89,103 +89,103 @@ $(document).ready(function() {
     function loopThroughSteps() {
       let step = i % 8;
 
-      let melodyStep = $(".melody .step:nth-child(" + (step+1) + ")");
-      if(melodyStep.hasClass("0")){
+      let melodyStep = $(".melody .step:nth-child(" + (step + 1) + ")");
+      if (melodyStep.hasClass("0")) {
         melody.releaseAll();
         melody.triggerAttack("D2");
-      } else if(melodyStep.hasClass("1")){
+      } else if (melodyStep.hasClass("1")) {
         melody.releaseAll();
         melody.triggerAttack("E2");
-      } else if(melodyStep.hasClass("2")){
+      } else if (melodyStep.hasClass("2")) {
         melody.releaseAll();
         melody.triggerAttack("F2");
-      } else if(melodyStep.hasClass("3")){
+      } else if (melodyStep.hasClass("3")) {
         melody.releaseAll();
         melody.triggerAttack("G2");
-      } else if(melodyStep.hasClass("4")){
+      } else if (melodyStep.hasClass("4")) {
         melody.releaseAll();
         melody.triggerAttack("A2");
-      } else if(melodyStep.hasClass("5")){
+      } else if (melodyStep.hasClass("5")) {
         melody.releaseAll();
         melody.triggerAttack("B2");
-      } else if(melodyStep.hasClass("6")){
+      } else if (melodyStep.hasClass("6")) {
         melody.releaseAll();
         melody.triggerAttack("C3");
-      } else if(melodyStep.hasClass("7")){
+      } else if (melodyStep.hasClass("7")) {
         melody.releaseAll();
         melody.triggerAttack("D3");
-      } else if(melodyStep.hasClass("8")){
+      } else if (melodyStep.hasClass("8")) {
         melody.releaseAll();
         melody.triggerAttack("E3");
-      } else if(melodyStep.hasClass("9")){
+      } else if (melodyStep.hasClass("9")) {
         melody.releaseAll();
         melody.triggerAttack("F3");
       }
 
-      let bassStep = $(".bass .step:nth-child(" + (step+1) + ")");
-      if(bassStep.hasClass("0")){
+      let bassStep = $(".bass .step:nth-child(" + (step + 1) + ")");
+      if (bassStep.hasClass("0")) {
         bass.releaseAll();
         bass.triggerAttack("D4");
-      } else if(bassStep.hasClass("1")){
+      } else if (bassStep.hasClass("1")) {
         bass.releaseAll();
         bass.triggerAttack("E4");
-      } else if(bassStep.hasClass("2")){
+      } else if (bassStep.hasClass("2")) {
         bass.releaseAll();
         bass.triggerAttack("F4");
-      } else if(bassStep.hasClass("3")){
+      } else if (bassStep.hasClass("3")) {
         bass.releaseAll();
         bass.triggerAttack("G4");
-      } else if(bassStep.hasClass("4")){
+      } else if (bassStep.hasClass("4")) {
         bass.releaseAll();
         bass.triggerAttack("A4");
-      } else if(bassStep.hasClass("5")){
+      } else if (bassStep.hasClass("5")) {
         bass.releaseAll();
         bass.triggerAttack("B4");
-      } else if(bassStep.hasClass("6")){
+      } else if (bassStep.hasClass("6")) {
         bass.releaseAll();
         bass.triggerAttack("C5");
-      } else if(bassStep.hasClass("7")){
+      } else if (bassStep.hasClass("7")) {
         bass.releaseAll();
         bass.triggerAttack("D5");
-      } else if(bassStep.hasClass("8")){
+      } else if (bassStep.hasClass("8")) {
         bass.releaseAll();
         bass.triggerAttack("E5");
-      } else if(bassStep.hasClass("9")){
+      } else if (bassStep.hasClass("9")) {
         bass.releaseAll();
         bass.triggerAttack("F5");
       }
 
-      let kickStep = $(".kick .step:nth-child(" + (step+1) + ")");
-      if(kickStep.hasClass("on")){
+      let kickStep = $(".kick .step:nth-child(" + (step + 1) + ")");
+      if (kickStep.hasClass("on")) {
         kick.volume.value = -18;
         kick.stop();
         kick.start();
       }
-      if(kickStep.hasClass("on-loud")){
+      if (kickStep.hasClass("on-loud")) {
         kick.volume.value = -9;
         kick.stop();
         kick.start();
       }
 
-      let snareStep = $(".snare .step:nth-child(" + (step+1) + ")");
-      if(snareStep.hasClass("on")){
+      let snareStep = $(".snare .step:nth-child(" + (step + 1) + ")");
+      if (snareStep.hasClass("on")) {
         snare.volume.value = -18;
         snare.stop();
         snare.start();
       }
-      if(snareStep.hasClass("on-loud")){
+      if (snareStep.hasClass("on-loud")) {
         snare.volume.value = -12;
         snare.stop();
         snare.start();
       }
 
-      let hihatStep = $(".hi-hat .step:nth-child(" + (step+1) + ")");
-      if(hihatStep.hasClass("on")){
+      let hihatStep = $(".hi-hat .step:nth-child(" + (step + 1) + ")");
+      if (hihatStep.hasClass("on")) {
         hihat.volume.value = -18;
         hihat.stop();
         hihat.start();
       }
-      if(hihatStep.hasClass("on-loud")){
+      if (hihatStep.hasClass("on-loud")) {
         hihat.volume.value = -9;
         hihat.stop();
         hihat.start();
@@ -193,74 +193,79 @@ $(document).ready(function() {
 
       console.log(step);
       i++;
-    }
+    };
 
-    function startTransport() {
-      Tone.Transport.start();
-      console.log("Transport started");
-    }
+      function startTransport() {
+        Tone.Transport.start();
+        console.log("Transport started");
+      }
 
-    function stopTransport() {
-      Tone.Transport.stop();
-      console.log("Transport stopped");
-    }
-
-    //defining that the transport will loop through 8 steps
-    Tone.Transport.scheduleRepeat(loopThroughSteps, "8n");
-  };
-
-  //play button starts/stops the transport
-  $("#play").click(function(){
-    if ($(this).hasClass('text-secondary')){
-      startTransport();
-      $(this).removeClass('text-secondary').addClass('text-white');
-    } else {
-      stopTransport();
-      $(this).removeClass('text-white').addClass('text-secondary');
-    }
-  });
+      function stopTransport() {
+        Tone.Transport.stop();
+        console.log("Transport stopped");
+      }
 
 
-  //when 16ths button is pressed, the steps are converted to 16th notes (double time)
-  $("#time-signature").click(function(){
-    if ($(this).hasClass('text-secondary')){
-      Tone.Transport.bpm.value = Tone.Transport.bpm.value * 2;
-      $(this).removeClass('text-secondary').addClass('text-white');
-    } else {
-      Tone.Transport.bpm.value = Tone.Transport.bpm.value / 2;
-      $(this).removeClass('text-white').addClass('text-secondary');
-    }
-  });
+      //defining that the transport will loop through 8 steps
+      Tone.Transport.scheduleRepeat(loopThroughSteps, "8n");
 
-  $("#tempo").click(function(){
-    let tempoValues = [
-      '144bpm',
-      '160bpm',
-      '180bpm',
-      '72bpm',
-      '90bpm',
-      '108bpm',
-      '120bpm'
-    ];
-    $(this).each(function(){
-      this.textContent = tempoValues[($.inArray(this.textContent, tempoValues)+1)%tempoValues.length];
-      Tone.Transport.bpm.value = parseInt(tempoValues[($.inArray(this.textContent, tempoValues))%tempoValues.length], 10);
-      console.log(Tone.Transport.bpm.value + 'bpm');
-    })
-  });
+
+
+
+      //play button starts/stops the transport
+      $("#play").click(function () {
+        if ($(this).hasClass('text-secondary')) {
+          startTransport();
+          $(this).removeClass('text-secondary').addClass('text-white');
+        } else {
+          stopTransport();
+          $(this).removeClass('text-white').addClass('text-secondary');
+        }
+      });
+
+
+      //when 16ths button is pressed, the steps are converted to 16th notes (double time)
+      $("#time-signature").click(function () {
+        if ($(this).hasClass('text-secondary')) {
+          Tone.Transport.bpm.value = Tone.Transport.bpm.value * 2;
+          $(this).removeClass('text-secondary').addClass('text-white');
+        } else {
+          Tone.Transport.bpm.value = Tone.Transport.bpm.value / 2;
+          $(this).removeClass('text-white').addClass('text-secondary');
+        }
+      });
+
+      $("#tempo").click(function () {
+        let tempoValues = [
+          '144bpm',
+          '160bpm',
+          '180bpm',
+          '72bpm',
+          '90bpm',
+          '108bpm',
+          '120bpm'
+        ];
+        $(this).each(function () {
+          this.textContent = tempoValues[($.inArray(this.textContent, tempoValues) + 1) % tempoValues.length];
+          Tone.Transport.bpm.value = parseInt(tempoValues[($.inArray(this.textContent, tempoValues)) % tempoValues.length], 10);
+          console.log(Tone.Transport.bpm.value + 'bpm');
+        })
+      });
+
+  }
 
   $("#save").click(() => {
     const steps = Array.from($('.step'));
-    let stepStates = steps.map( element => {
+    let stepStates = steps.map(element => {
       return element.className;
     })
     console.log(stepStates);
-    
+
     $("#lower-section").append($("<div></div>").text("'Save' feature under construction...").addClass("save-form p-2").attr('id', 'under-construction'));
   });
 
   $("#lower-section").click((event) => {
-    if(event.target.id === 'under-construction'){
+    if (event.target.id === 'under-construction') {
       $("#under-construction").remove();
     };
   });
